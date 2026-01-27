fd480d002735d6246f1c5c01647f4e352b76e97f
Adarsh <adarshrathore.sec@gmail.com>
2026-01-27 14:58:29 +0530

    async in javascript

--- script.js
+++ script.js
@@ -1,47 +1,74 @@
-// // Lecture1
-// // const para= document.querySelector("p")
-// // para.innerText = "This is a New paragraph"
-// // para.style.colorv= "green"
+// // // Lecture1
+// // // const para= document.querySelector("p")
+// // // para.innerText = "This is a New paragraph"
+// // // para.style.colorv= "green"
+// // // console.log(para)
+
+
+// // // Lecture2and3
+// // // console.log(document)
+// // // const para = document.getElementById("para")
+// // // para.innerText = "hiii"
+
+// // const para = document.getElementsByClassName("para")
+// // console.log(para)
+// // para[0].innerText = "This is a new paragrpah"
+// // para[1].computedStyleMap.color = "green"
 // // console.log(para)
 
+// // let arr=[23,144,345,34,34,24,3,34]
 
-// // Lecture2and3
-// // console.log(document)
-// // const para = document.getElementById("para")
-// // para.innerText = "hiii"
-
-// const para = document.getElementsByClassName("para")
-// console.log(para)
-// para[0].innerText = "This is a new paragrpah"
-// para[1].computedStyleMap.color = "green"
-// console.log(para)
-
-// let arr=[23,144,345,34,34,24,3,34]
-
-// //const newA=arr.map((ele)=>ele) //it returns new array
+// // //const newA=arr.map((ele)=>ele) //it returns new array
 
 
-function message()
-{
-    alert("You have clicked the button")
-}
+// function message()
+// {
+//     alert("You have clicked the button")
+// }
 
-const btn = document.querySelector("button")
-btn.classList.add("btn") //adding css file
+// const btn = document.querySelector("button")
+// btn.classList.add("btn") //adding css file
 
-// btn.classList.remove("btn")
-// btn.addEventListener('mouseover',function(){alert("You have clicked the button")})
-// btn.addEventListener('click',function(){close("You have clicked the button")})
+// // btn.classList.remove("btn")
+// // btn.addEventListener('mouseover',function(){alert("You have clicked the button")})
+// // btn.addEventListener('click',function(){close("You have clicked the button")})
 
-btn.addEventListener('click', message) //to add
-btn.removeEventListener('click', message) //to remove
+// btn.addEventListener('click', message) //to add
+// btn.removeEventListener('click', message) //to remove
 
 
 //20 jan 2026 - lecture7
-const container=document.querySelector('.container')
-const outer=document.querySelector('.outer')
-const button=document.querySelector('button')
+// const container=document.querySelector('.container')
+// const outer=document.querySelector('.outer')
+// const button=document.querySelector('button')
 
-container.addEventListener('click',()=>{console.log("Div")},true)
-outer.addEventListener('click',()=>{console.log("Outer Div")},true)
-button.addEventListener('click',()=>{console.log("Button")},true)
+// container.addEventListener('click',()=>{console.log("Div")},true)
+// outer.addEventListener('click',()=>{console.log("Outer Div")},true)
+// button.addEventListener('click',()=>{console.log("Button")},true)
+
+
+
+// lecture-8
+
+// console.log(b)
+// console.log(a)
+
+// let a=23456
+// var b=3456
+
+// console.log(a)
+// console.log(b)
+// function print(){
+//     console.log("Inside fn")
+// }
+
+// print()
+
+
+// lecture-9
+
+console.log("first line")
+setTimeout(() => {
+    
+}, timeout);
+console.log("Second line")
